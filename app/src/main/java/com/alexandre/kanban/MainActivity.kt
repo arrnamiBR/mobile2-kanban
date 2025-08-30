b1e2f417af02484ee7b8e014da2d52682c4a919a
Alexandre Guerra <alexandre.guerra765@gmail.com>
2025-08-30 17:20:32 -0300

    parte 1

---
+++ app/src/main/java/com/alexandre/kanban/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.alexandre.kanban
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.alexandre.kanban.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+        setContentView(R.layout.activity_main)
+
+    }
+}
