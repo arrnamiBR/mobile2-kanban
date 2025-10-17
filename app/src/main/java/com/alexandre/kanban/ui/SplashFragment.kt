1b5fa1df8149a7c7f0a82a6696e0fed8b8b56a97
Alexandre <alexandre.guerra765@gmail.com>
2025-10-17 08:46:22 -0300

    tela de splash modificada

--- app/src/main/java/com/alexandre/kanban/ui/SplashFragment.kt
+++ app/src/main/java/com/alexandre/kanban/ui/SplashFragment.kt
@@ -7,15 +7,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.alexandre.kanban.R
 import com.alexandre.kanban.databinding.FragmentSplashBinding
+import com.google.firebase.auth.FirebaseAuth
 
 
 class SplashFragment : Fragment() {
 
     private var _binding: FragmentSplashBinding? = null
     private val binding get() = _binding!!
+
+    lateinit var auth: FirebaseAuth
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -29,11 +33,27 @@ class SplashFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        auth = FirebaseAuth.getInstance()
+
         Handler(Looper.getMainLooper()).postDelayed({checkAuth()}, 3000)
     }
 
     private fun checkAuth() {
-        findNavController().navigate(R.id.action_splashFragment_to_autentication)
+        try {
+            val user = auth.currentUser
+
+            if (user != null) {
+                findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+            }
+            else {
+                findNavController().navigate(R.id.action_splashFragment_to_autentication)
+            }
+        }
+        catch (e: Exception) {
+            Toast.makeText(requireContext(), e.message.toString(), Toast.LENGTH_SHORT).show()
+            findNavController().navigate(R.id.action_splashFragment_to_autentication)
+        }
+
     }
 
     override fun onDestroyView() {
